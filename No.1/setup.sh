ff468d8b79b28905a481477b58b290a133b63691
394498036@qq.com <394498036@qq.com>
2018-06-02 15:30:27 +0800

    增加权限ssh

--- No.1/setup.sh
+++ No.1/setup.sh
@@ -10,4 +10,13 @@ sh get-docker.sh
 # start docker service
 sudo systemctl start docker
 
+sudo groupadd docker
+
+sudo gpasswd -a $USER docker
+
+newgrp docker
+
+sudo service docker restart
+
+
 rm -rf get-docker.sh
