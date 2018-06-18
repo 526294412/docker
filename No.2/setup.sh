32bcb7fdc810cea32a8df377484d9a3049b077ef
394498036@qq.com <394498036@qq.com>
2018-06-18 17:18:05 +0800

    ip地址修改

--- No.2/setup.sh
+++ No.2/setup.sh
@@ -1,4 +1,10 @@
 #/bin/sh
+# yum install yum upgrade
+# yum install net-tools
+
+sudo yum install -y yum upgrade
+
+sudo yum install -y net-tools
 
 # install some tools
 sudo yum install -y git vim gcc glibc-static telnet bridge-utils
