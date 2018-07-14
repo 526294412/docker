b2cc8cd37c91083ac9d4d354c9593743504ef644
394498036@qq.com <394498036@qq.com>
2018-07-14 23:48:02 +0800

    增加docker学习第5章源码

---
+++ No.5/setup.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+# install some tools
+sudo yum install -y git vim gcc glibc-static telnet bridge-utils
+
+# install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# start docker service
+sudo groupadd docker
+sudo gpasswd -a vagrant docker
+sudo systemctl start docker
+
+rm -rf get-docker.sh
+
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://b81aace9.m.daocloud.io
